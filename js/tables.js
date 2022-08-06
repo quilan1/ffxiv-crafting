159bf397b4a80318ee30ef9852e6fb0069612527
John Reynolds <quilan@gmail.com>
2022-08-06 12:35:33 -0400

    Now saves, loads & deletes filters for custom items

--- js/tables.js
+++ js/tables.js
@@ -1,49 +1,46 @@
-const tables = {};
-const tablesProto = Object.getPrototypeOf(tables);
-
-tablesProto.createTable = (columns, rows) => {
-    var _table = document.createElement("table");
-    var _thead = _createTableHeader(columns);
-    var _tbody = _createTableRows(rows);
-    if (_thead) _table.appendChild(_thead);
-    _table.appendChild(_tbody);
-    return _table;
-}
-
-_createTableHeader = (columns) => {
-    if (!columns) {
-        return null;
+const tables = (() => {
+    this.createTable = (columns, rows) => {
+        var _table = document.createElement("table");
+        var _thead = _createTableHeader(columns);
+        var _tbody = this.createRows(rows);
+        if (_thead) _table.appendChild(_thead);
+        _table.appendChild(_tbody);
+        return _table;
     }
 
-    var _thead = document.createElement("thead");
-    var _tr = document.createElement("tr");
-    for (const column of columns) {
-        const _th = document.createElement("th");
-        const _text = document.createTextNode(column);
-        _th.appendChild(_text);
-        _tr.appendChild(_th);
+    this.createRows = rows => {
+        return util.makeElem({ tag: 'tbody', children: rows.map(row => this.createRow(row))});
     }
-    _thead.appendChild(_tr);
-    return _thead;
-}
 
-_createTableRows = (rows) => {
-    var _tbody = document.createElement("tbody");
-    for (const row of rows) {
-        const _tr = document.createElement("tr");
+    this.createRow = row => {
+        const tr = util.makeElem({ tag: 'tr' });
         for (const data of row) {
-            const _td = document.createElement("td");
+            const td = util.makeElem({ tag: 'td' });
             if (typeof data === "string" || typeof data === "number") {
-                const _text = document.createTextNode(data.toString());
-                _td.appendChild(_text);
+                td.innerText = data.toString();
             } else if (typeof data === "function") {
-                data(_td, _tr);
+                data(td, tr);
             } else {
-                _td.appendChild(data);
+                td.appendChild(data);
             }
-            _tr.appendChild(_td);
+            tr.appendChild(td);
         }
-        _tbody.appendChild(_tr);
+        return tr;
     }
-    return _tbody;
-}
+
+    _createTableHeader = (columns) => {
+        if (!columns) {
+            return null;
+        }
+
+        return util.makeElem({
+            tag: 'thead',
+            children: [{
+                tag: 'tr',
+                children: columns.map(column => ({ tag: 'th', innerText: column }))
+            }]
+        });
+    }
+
+    return this;
+})();
