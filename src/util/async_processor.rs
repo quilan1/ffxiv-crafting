e8460c8e3cc4e5cd524cb206c1e42957a30eef53
John Reynolds <quilan@gmail.com>
2022-12-04 15:14:52 -0500

    Cleaned up & documented more of the processor code

--- src/util/async_processor.rs
+++ src/util/async_processor.rs
@@ -9,6 +9,7 @@ use futures::{
     future::{join_all, BoxFuture, Shared},
     Future, FutureExt,
 };
+use log::error;
 use parking_lot::Mutex;
 
 #[allow(unused_imports)]
@@ -28,12 +29,10 @@ struct NotifyFuture<O> {
     counter: AsyncCounter,
 }
 
-#[allow(dead_code)]
 struct AsyncProcessorData {
     active: Vec<BoxFuture<'static, ()>>,
     queue: VecDeque<BoxFuture<'static, ()>>,
     waker: Option<Waker>,
-    name: String,
     max_queue: usize,
 }
 
@@ -45,18 +44,18 @@ pub trait Notify {
 
 // Consumer side of the API
 impl AsyncProcessor {
-    pub fn new<S: Into<String>>(name: S, max_queue: usize) -> Self {
+    pub fn new(max_queue: usize) -> Self {
         Self {
             data: Arc::new(Mutex::new(AsyncProcessorData {
                 active: Vec::new(),
                 queue: VecDeque::new(),
                 waker: None,
-                name: name.into(),
                 max_queue,
             })),
         }
     }
 
+    // Takes a vector of shared futures, and sends the through the count-limited queue and yields the results
     pub async fn process<O>(&self, values: Vec<SharedFuture<O>>) -> Vec<O>
     where
         O: Clone + Send + Sync + 'static,
@@ -72,6 +71,7 @@ impl AsyncProcessor {
         join_all(values).await
     }
 
+    // Adds the futures to the internal queue system of the AsyncProcessor
     fn queue_futures<O>(&self, futures: Vec<SharedFuture<O>>, counter: AsyncCounter)
     where
         O: Clone + Send + Sync + 'static,
@@ -80,21 +80,26 @@ impl AsyncProcessor {
 
         // Move the futures into the queue
         for future in futures {
-            let notified_future = NotifyFuture {
-                future,
-                counter: counter.clone(),
-            }
-            .boxed();
-            data.queue.push_back(notified_future);
+            data.queue.push_back(
+                NotifyFuture {
+                    future,
+                    counter: counter.clone(),
+                }
+                .boxed(),
+            );
         }
 
-        // Poll processor again
+        // Wake up the processor, so it can take a look at the queue & move them into active polling
         if let Some(waker) = data.waker.as_ref() {
             waker.wake_by_ref();
+        } else {
+            error!("AsyncProcessor waker does not exist?! This usually means the processor is not currently\
+                   'await'ing somewhere. Might cause a zombie future.");
         }
     }
 }
 
+// Abstract out the notify future polling
 impl<O: Clone> Future for NotifyFuture<O> {
     type Output = ();
 
@@ -119,8 +124,7 @@ impl Future for AsyncProcessor {
         let mut data = self.data.lock();
 
         // info!(
-        //     "[Poll {}] Start (active={}, queue={})",
-        //     data.name,
+        //     "[Poll] Start (active={}, queue={})",
         //     data.active.len(),
         //     data.queue.len()
         // );
@@ -136,16 +140,16 @@ impl Future for AsyncProcessor {
         // Filter out any completed futures
         let mut notified_futures = data.active.drain(..).collect::<Vec<_>>();
         for mut notified_future in notified_futures.drain(..) {
-            if Pin::new(&mut notified_future).poll(cx) == Poll::Pending {
+            if notified_future.as_mut().poll(cx) == Poll::Pending {
                 data.active.push(notified_future);
             }
         }
 
         // Set the waker, so it can be re-polled
         data.waker = Some(cx.waker().clone());
+
         // info!(
-        //     "[Poll {}] Done (active={}, queue={})",
-        //     data.name,
+        //     "[Poll] Done (active={}, queue={})",
         //     data.active.len(),
         //     data.queue.len()
         // );
