09d6cc2ccf7114c5f3762e9dea7015dd08bf8063
John Reynolds <quilan@gmail.com>
2023-09-10 10:06:59 -0400

    Further reduced public visibility of ffxiv_items

--- ffxiv_items/src/lib.rs
+++ ffxiv_items/src/lib.rs
@@ -5,11 +5,10 @@ mod parsers;
 mod recipe;
 mod util;
 
+use filter::Filter;
 use library::library;
-use recipe::Ingredient;
+use recipe::{Ingredient, Recipe};
 
-pub use filter::Filter;
 pub use item_info::ItemInfo;
 pub use library::Library;
-pub use recipe::Recipe;
 pub use util::{get_ids_from_filters, item_name};
