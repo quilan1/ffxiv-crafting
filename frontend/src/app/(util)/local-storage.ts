e94ef0587ce692a0ff434f8790a980ab255b3efd
John Reynolds <quilan@gmail.com>
2023-11-08 21:07:23 -0500

    [frontend] Improved localStorage code a bit

--- frontend/src/app/(util)/local-storage.ts
+++ frontend/src/app/(util)/local-storage.ts
@@ -3,18 +3,47 @@ import { SimpleSetter } from "./signal";
 
 export function useLocalStorageGet<T>(name: string, setState: SimpleSetter<T>) {
     useEffect(() => {
-        try {
+        if (localStorageAvailable()) {
             const value = localStorage.getItem(name);
-            if (value != undefined) setState(value as T);
-        } catch { }
+            if (value != null) setState(value as T);
+        }
     }, [setState, name]);
 }
 
 export function useLocalStorageSet<T>(name: string, setState?: SimpleSetter<T>): SimpleSetter<T> {
     return (value: T) => {
-        try {
-            localStorage.setItem(name, value as string)
-        } catch { }
+        if (localStorageAvailable()) {
+            localStorage.setItem(name, value as string);
+        }
         if (setState) setState(value);
     };
+}
+
+const localStorageAvailable = () => storageAvailable("localStorage");
+
+function storageAvailable(type: string) {
+    let storage: Storage | undefined;
+    try {
+        storage = (window as unknown as Record<string, Storage>)[type];
+        const x = "__storage_test__";
+        storage.setItem(x, x);
+        storage.removeItem(x);
+        return true;
+    } catch (e) {
+        return (
+            e instanceof DOMException &&
+            // everything except Firefox
+            (e.code === 22 ||
+                // Firefox
+                e.code === 1014 ||
+                // test name field too, because code might not be present
+                // everything except Firefox
+                e.name === "QuotaExceededError" ||
+                // Firefox
+                e.name === "NS_ERROR_DOM_QUOTA_REACHED") &&
+            // acknowledge QuotaExceededError only if there's something already stored
+            storage &&
+            storage.length !== 0
+        );
+    }
 }
